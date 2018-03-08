79914253fc0ca9171d675c0dd3d3ca80b4e6c174
Christine Cua <cloudycua@gmail.com>
2018-03-08 03:46:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,11 +10,11 @@ class Dog
   end
 
   def self.all
-    @all.each { |dog| puts dog.name }
+    @@all.each { |dog| puts dog.name }
   end
 
   def self.clear_all
-    @all.delete
+    @@all.delete
   end
 
 end
